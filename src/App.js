b15ec16a366135798bb7e3ea18ac03431c06acec
Andrew Park <atpark333@gmail.com>
2021-05-25 15:53:59 +0900

    refactor: Remove default group array

--- src/App.js
+++ src/App.js
@@ -4,23 +4,8 @@ import "./App.css";
 import Add from "./Add";
 import { GroupList } from "./Group";
 
-const defaultList = [
-  {
-    name: "Curtis",
-    id: 1,
-  },
-  {
-    name: "Yukari",
-    id: 2,
-  },
-  {
-    name: "Daichi",
-    id: 3,
-  },
-];
-
 function App() {
-  const [members, setMembers] = useState(defaultList);
+  const [members, setMembers] = useState([]);
   const [total, setTotal] = useState(0);
 
   const handleChange = e => {
